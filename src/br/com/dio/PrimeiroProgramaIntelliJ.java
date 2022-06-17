d682b8bc55c45ce8f391907a4febb997b9141f68
CKonno <claudiane.konno@gmail.com>
2022-06-17 19:44:08 -0300

    first commit

--- src/br/com/dio/PrimeiroProgramaIntelliJ.java
+++ src/br/com/dio/PrimeiroProgramaIntelliJ.java
@@ -8,6 +8,42 @@ public class PrimeiroProgramaIntelliJ {
 
         Gato gato = new Gato();
         System.out.println(gato);
+        Livro livro1 = new Livro("O problema dos 3 corpos", 300);
+        System.out.println(livro1);
     }
 
+}
+
+class Livro{
+    private String nome;
+    private Integer npag;
+
+    public Livro(String nome, Integer npag) {
+        this.nome = nome;
+        this.npag = npag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", npag=" + npag +
+                '}';
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNpag() {
+        return npag;
+    }
+
+    public void setNpag(Integer npag) {
+        this.npag = npag;
+    }
 }
